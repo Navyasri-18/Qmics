d37a10a4d7fedbf891b076fb2942917d82762c0a
Navyasri Kamble <nitin@MacBook-Pro.local>
2026-02-16 17:19:45 +0530

    deployed frontend

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:3000",
-  // Add your deployed frontend URL here when available
+  "qmics.vercel.app",
 ];
 
 app.use(
